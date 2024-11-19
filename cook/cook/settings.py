32f839f105351eb6817188e3a9f3b7dc724c738b
BiologPDA <kozar.pavel.007@gmail.com>
2024-11-19 18:22:56 +0100

    supernew version with comfortable design

--- cook/cook/settings.py
+++ cook/cook/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'webappdb',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'resistol007',
         'HOST': 'localhost',
         'PORT': '5432',
     }

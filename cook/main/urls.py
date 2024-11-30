13387d23dacfe5a8f277c70b895ffd3c74fe9db1
liberator14-arch <cn.jersey14@gmail.com>
2024-11-30 20:16:07 +0300

    Обновление, также прошу добавить куда нибудь версию проекта, например v1.0.1

--- cook/main/urls.py
+++ cook/main/urls.py
@@ -3,18 +3,48 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name='home'),
-    path('menu', views.menu, name='menu'),
-    path('about', views.about, name='about'),
-    path('gallery', views.gallery, name='gallery'),
-    path('contacts', views.contacts, name='contacts'),
-    path('reservation', views.reservation, name='reservation'),
-    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),  # Страница входа
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Страница выхода
-    path('register/', views.register, name='register'),  # Страница регистрации
-    path('dishes', views.dishes, name='dishes'),
-    path('main_dish', views.main_dish, name='main_dish'),
-    path('bar', views.bar, name='bar'),
-    path('desserts', views.desserts, name='desserts'),
+    # Главная страница
+    path('home/', views.index, name='home'),  
+
+    # Меню
+    path('menu/', views.menu, name='menu'),  
+
+    # О нас
+    path('about/', views.about, name='about'),  
+
+    # Галерея
+    path('gallery/', views.gallery, name='gallery'),  
+
+    # Контакты
+    path('contacts/', views.contacts, name='contacts'),  
+
+    # Бронирование
+    path('reservation/', views.reservation_view, name='reservation'),
+
+    # Вход
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+
+    # Выход (можно использовать стандартный или ваш кастомный)
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),  # Страница выхода
+
+    # Регистрация
+    path('register/', views.register, name='register'),
+    # Блюда
+    path('dishes/', views.dishes, name='dishes'),
+
+    # Основные блюда
+    path('main_dish/', views.main_dish, name='main_dish'),
+
+    # Бар
+    path('bar/', views.bar, name='bar'),
+
+    # Десерты
+    path('desserts/', views.desserts, name='desserts'),
+
+    # Профиль
     path('profile/', views.profile_view, name='profile'),
+    path('', views.home_view, name='home'),
+    path('', views.index, name='index'),
+
 ]
